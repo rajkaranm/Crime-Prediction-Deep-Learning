ae0a803ab745539beb940efcfb08183cef19f523
rajkaranm <rajkaranvir456@gmail.com>
2024-04-06 14:50:53 +0530

    removed route problem

--- Website/core/urls.py
+++ Website/core/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('stream', views.HomeView, name='home'),
+    path('', views.HomeView, name='home'),
     path('getstream', views.index, name='streamdt'),
     path('stream', views.StreamView, name='streamroom'),
    ]
